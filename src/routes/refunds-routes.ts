45f3ae94b1177f19912773b5aeb8d5b27a10fe21
AndreTrevizam <andretrevizam264@gmail.com>
2025-05-12 22:02:45 -0300

    feat: refactor refunds routes for improved readability and structure

--- src/routes/refunds-routes.ts
+++ src/routes/refunds-routes.ts
@@ -8,6 +8,19 @@ const refundsController = new RefundsController()
 refundsRoutes.post(
   "/",
   verifyUserAuthorization(['employee']),
-  refundsController.create)
+  refundsController.create
+)
+
+refundsRoutes.get(
+  "/",
+  verifyUserAuthorization(['manager']),
+  refundsController.index
+)
+
+refundsRoutes.get(
+  "/:id",
+  verifyUserAuthorization(['employee', 'manager']),
+  refundsController.show
+)
 
 export { refundsRoutes }
